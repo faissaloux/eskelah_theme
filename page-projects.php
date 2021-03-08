56a51c032c0f8308664b1a59257274c4701c0214
MedAkou <akoutoss.med@gmail.com>
2021-03-08 13:30:31 +0000

    up

--- page-projects.php
+++ page-projects.php
@@ -8,7 +8,7 @@
 <?php global $vectors;  ?>
 
 <!-- جلب محتوى الهيدر -->
-<?php get_main_header(); ?>  
+<?php get_main_header(); ?>
 
 <?php
     $projectsCategories = get_terms( array(
@@ -18,27 +18,27 @@
 ?>
 
 <style>
-    @media only screen and (min-width: 760px) {
+@media only screen and (min-width: 760px) {
 
-        .col-md-6.col-md-offset-3.col-sm-12.text-center {
-            margin: 0 auto !important;
-            display: inline-block;
-            width: initial;
-            float: none;
-        }
-
-        section.service-tite.projects_title {
-            text-align: center;
-        }
-            
-
-        .postitle h1 {
-            margin-top: 105px;
-            font-size: 38px;
-        }
+    .col-md-6.col-md-offset-3.col-sm-12.text-center {
+        margin: 0 auto !important;
+        display: inline-block;
+        width: initial;
+        float: none;
     }
+
+    section.service-tite.projects_title {
+        text-align: center;
+    }
+
+
+    .postitle h1 {
+        margin-top: 105px;
+        font-size: 38px;
+    }
+}
 </style>
-    <?php  
+<?php  
         $args = array(
             'numberposts' => -1,
             'post_type'        => 'projects',
@@ -50,253 +50,267 @@
         );
         $projects = get_posts( $args );  
     ?>
-    <!-- about us eskelah team -->
-    <section class="service-tite projects_title">
-        <div class="col-md-6 col-md-offset-3 col-sm-12 text-center">
-           <img src="<?php echo $vectors; ?>/project/Groupe%2061.png" alt="">
-           <div class="page-title">
+<!-- about us eskelah team -->
+<section class="service-tite projects_title">
+    <div class="col-md-6 col-md-offset-3 col-sm-12 text-center">
+        <img src="<?php echo $vectors; ?>/project/Groupe%2061.png" alt="">
+        <div class="page-title">
             <h1 class="main-title">
                 <span>في</span>
                 <span class="eskelah undelined">إسكلة</span>
                 <span>لا نتجاوز التحديات وحسب</span>
             </h1>
             <h3 class="text-right">بل نرفق بأحلامك في مرفأ الإبداع </h3>
-            </div>
-        </div>
-    </section>
-    <!--// about us eskelah team -->
-
-    <section class="categories-switchers flex-row hidden-xs">
-        <div class="categories-switcher active" data-category="0">
-            <a href="javascript:;">
-            الكل
-            </a>
-        </div>
-        <?php foreach($projectsCategories as $category): ?>
-            <div class="categories-switcher" data-category="<?php echo $category->term_id; ?>">
-                <a href="javascript:;">
-                <?php echo $category->name; ?>
-                </a>
-            </div>
-        <?php endforeach; ?>
-    </section>
-
-    <style>
-        .map-responsive{
-            overflow:hidden;
-            padding-bottom:56.25%;
-            position:relative;
-            height:0;
-        }
-        .map-responsive iframe{
-            left:0;
-            top:0;
-            height:100%;
-            width:100%;
-            position:absolute;
-        }
-        .frame {
-            height:270px !important;
-            width:640px !important;
-            margin:0 auto;
-            background-size: contain !important;
-            width: 100%;
-            background-repeat: no-repeat !important;
-        }
-
-        section.portfolio {
-            max-width: 1340px;
-            margin: 0 auto;
-        }
-
-        .frame:after {
-            content:'dd';
-            top:0;
-            left:0;
-            content:'';
-            right:30px;
-            bottom:0;
-            background:red;
-            position:absolute;
-            -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
-            filter: grayscale(100%);
-            background-size: contain !important;
-        }
-
-        .frame {
-            position:relative;
-            height:270px !important;
-            width:640px !important;
-            margin:0 auto;
-            width: 100%;
-            
-        }
-
-        .project-info {
-            text-align: right;
-            width:640px !important;
-            margin:0 auto;
-            padding-right:45px;
-        }
-
-        .frame a.btn {
-            color: white;
-            text-decoration: none;
-            padding: 15px 28px;
-            display: inline-block;
-            float: left;
-            font-weight: bold;
-            background: #0000008c;
-            top: 110px !important;
-            position: absolute;
-            z-index: 9999999;
-            left: 253px;
-            color: white;
-            border: 2px solid #FFBE2E !important;
-        }
-        .frame {
-            margin-bottom: 25px;
-        }
-        .frame img {
-            -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
-            filter: grayscale(100%);
-            width: 100%;
-        }
-
-        .frame {
-            overflow: hidden;
-        }
-
-        .frame:after {
-            display: none;
-        }
-        .frame img:hover {
-            filter: initial;
-        }
-    </style>
-  
-    <div class="modal fade" id="video_project" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-body"></div>
-            </div>
         </div>
     </div>
- 
+</section>
+<!--// about us eskelah team -->
+
+<section class="categories-switchers flex-row hidden-xs">
+    <div class="categories-switcher active" data-category="0">
+        <a href="javascript:;">
+            الكل
+        </a>
+    </div>
+    <?php foreach($projectsCategories as $category): ?>
+    <div class="categories-switcher" data-category="<?php echo $category->term_id; ?>">
+        <a href="javascript:;">
+            <?php echo $category->name; ?>
+        </a>
+    </div>
+    <?php endforeach; ?>
+</section>
+
+<style>
+.map-responsive {
+    overflow: hidden;
+    padding-bottom: 56.25%;
+    position: relative;
+    height: 0;
+}
+
+.map-responsive iframe {
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+}
+
+.frame {
+    height: 270px !important;
+    width: 640px !important;
+    margin: 0 auto;
+    background-size: contain !important;
+    width: 100%;
+    background-repeat: no-repeat !important;
+}
+
+section.portfolio {
+    max-width: 1340px;
+    margin: 0 auto;
+}
+
+.frame:after {
+    content: 'dd';
+    top: 0;
+    left: 0;
+    content: '';
+    right: 30px;
+    bottom: 0;
+    background: red;
+    position: absolute;
+    -webkit-filter: grayscale(100%);
+    /* Safari 6.0 - 9.0 */
+    filter: grayscale(100%);
+    background-size: contain !important;
+}
+
+.frame {
+    position: relative;
+    height: 270px !important;
+    width: 640px !important;
+    margin: 0 auto;
+    width: 100%;
+
+}
+
+.project-info {
+    text-align: right;
+    width: 640px !important;
+    margin: 0 auto;
+    padding-right: 45px;
+}
+
+.frame a.btn {
+    color: white;
+    text-decoration: none;
+    padding: 15px 28px;
+    display: inline-block;
+    float: left;
+    font-weight: bold;
+    background: #0000008c;
+    top: 110px !important;
+    position: absolute;
+    z-index: 9999999;
+    left: 253px;
+    color: white;
+    border: 2px solid #FFBE2E !important;
+}
+
+.frame {
+    margin-bottom: 25px;
+}
+
+.frame img {
+    -webkit-filter: grayscale(100%);
+    /* Safari 6.0 - 9.0 */
+    filter: grayscale(100%);
+    width: 100%;
+}
+
+.frame {
+    overflow: hidden;
+}
+
+.frame:after {
+    display: none;
+}
+
+.frame img:hover {
+    filter: initial;
+}
+</style>
+
+<div class="modal fade" id="video_project" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body"></div>
+        </div>
+    </div>
+</div>
+
 <section class="portfolio">
     <div class="col-md-12">
 
         <?php $count = 0; foreach($projects as $post): setup_postdata($post);  ?>
-        
+
         <?php 
             // Get the metaBoxes 
             $meta_data          = get_post_meta( get_the_ID(), 'project_info', true );
             $little_description = $meta_data['little_description'];
             $video_link         = $meta_data['video_link'];
         ?>
-    
-    <!-- style='background:url("<?php echo urldecode(get_the_post_thumbnail_url()) ;?> ") !important; '-->
-    <div class="col-md-6 hidden-xs">
-        <div class='frame' >
-            <img src='<?php echo urldecode(get_the_post_thumbnail_url()) ;?>' />
-            <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project" data-video="<?php echo $video_link ?>">
-            <?php if($video_link): ?>
-                <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png"/></span>
-            <?php endif; ?>
-            </a>
+
+        <!-- style='background:url("<?php echo urldecode(get_the_post_thumbnail_url()) ;?> ") !important; '-->
+        <div class="col-md-6 hidden-xs">
+            <div class='frame'>
+                <img src='<?php echo urldecode(get_the_post_thumbnail_url()) ;?>' />
+
+
+                <!-- class="show-project-video" data-toggle="modal" data-target="#video_project" data-video="<?php echo $video_link ?>" !-->
+                <a href="<?php the_permalink(); ?>">
+                    <?php if($video_link): ?>
+                    <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png" /></span>
+                    <?php endif; ?>
+                </a>
+            </div>
         </div>
-    </div>
-    
+
         <?php $class = ($count % 2 == 1) ? "odd" : "even"; ?>
-            <div class="col-md-8 col-md-offset-2  col-sm-8 projectDisplay <?php echo $class; ?> hidden-sm hidden-lg hidden-md">
-                <div class="main_display">
-                
+        <div
+            class="col-md-8 col-md-offset-2  col-sm-8 projectDisplay <?php echo $class; ?> hidden-sm hidden-lg hidden-md">
+            <div class="main_display">
+
                 <?php if($class == 'odd'): ?>
-                    <div class="project-info-dipslay">
-                        <h2>
-                            <?php the_title(); ?>
-                        </h2>
-                        <pre><?php echo $little_description; ?></pre>
-                        <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project" data-video="<?php echo $video_link ?>">
-                            <?php if($video_link): ?>
-                                <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png"/></span>
-                            <?php endif; ?>
-                        </a>
-                    </div>
-                    <div class="project-display-thumbnail">
-                    <a href="<?php the_permalink(); ?>">
-                        <img src="<?php echo get_the_post_thumbnail_url() ;?>" alt="">
-                        </a>
+                <div class="project-info-dipslay">
+                    <h2>
+                        <?php the_title(); ?>
+                    </h2>
+                    <pre><?php echo $little_description; ?></pre>
+                    <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project"
+                        data-video="<?php echo $video_link ?>">
+                        <?php if($video_link): ?>
+                        <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png" /></span>
+                        <?php endif; ?>
+                    </a>
                 </div>
-                
-                
-                    <?php else : ?>
-                
-                    <div class="project-info-dipslay hidden-sm hidden-md hidden-lg">
-                        <h2>
-                            <?php the_title(); ?>
-                        </h2>
-                        <pre><?php echo $little_description; ?></pre>
-                        <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project" data-video="<?php echo $video_link ?>">
-                            <?php if($video_link): ?>
-                                <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png"/></span>
-                            <?php endif; ?>
-                        </a>
-                    </div>
-                    <div class="project-display-thumbnail hidden-sm hidden-md hidden-lg">
+                <div class="project-display-thumbnail">
                     <a href="<?php the_permalink(); ?>">
                         <img src="<?php echo get_the_post_thumbnail_url() ;?>" alt="">
                     </a>
                 </div>
-                
-                
-                
-                
+
+
+                <?php else : ?>
+
+                <div class="project-info-dipslay hidden-sm hidden-md hidden-lg">
+                    <h2>
+                        <?php the_title(); ?>
+                    </h2>
+                    <pre><?php echo $little_description; ?></pre>
+                    <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project"
+                        data-video="<?php echo $video_link ?>">
+                        <?php if($video_link): ?>
+                        <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png" /></span>
+                        <?php endif; ?>
+                    </a>
+                </div>
+                <div class="project-display-thumbnail hidden-sm hidden-md hidden-lg">
+                    <a href="<?php the_permalink(); ?>">
+                        <img src="<?php echo get_the_post_thumbnail_url() ;?>" alt="">
+                    </a>
+                </div>
+
+
+
+
                 <div class="project-display-thumbnail hidden-xs">
                     <a href="<?php the_permalink(); ?>">
                         <img src="<?php echo get_the_post_thumbnail_url() ;?>" alt="">
-                        </a>
+                    </a>
                 </div>
-                    <div class="project-info-dipslay hidden-xs">
-                        <h2>
-                            <?php the_title(); ?>
-                        </h2>
-                        <pre><?php echo $little_description; ?></pre>
-                        <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project" data-video="<?php echo $video_link ?>">
-                            <?php if($video_link): ?>
-                                <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png"/></span>
-                            <?php endif; ?>
-                        </a>
-                    </div>
-                
-                    
+                <div class="project-info-dipslay hidden-xs">
+                    <h2>
+                        <?php the_title(); ?>
+                    </h2>
+                    <pre><?php echo $little_description; ?></pre>
+                    <a href="javascript:;" class="show-project-video" data-toggle="modal" data-target="#video_project"
+                        data-video="<?php echo $video_link ?>">
+                        <?php if($video_link): ?>
+                        <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png" /></span>
+                        <?php endif; ?>
+                    </a>
+                </div>
+
+
                 <?php endif; ?>
-                
-                </div>
-            
+
             </div>
-        <?php $count++; endforeach; ?> 
-    </div>    
+
+        </div>
+        <?php $count++; endforeach; ?>
+    </div>
 </section>
 
 
-  
-  
-   
 
 
 
 
 
-   
-    
-    
-    
-    
-    
-    
-   
- 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php get_simple_contact(); ?>
--- single-projects.php
+++ single-projects.php
@@ -6,7 +6,7 @@
 <?php global $vectors;  ?>
 
 <!-- جلب محتوى الهيدر -->
-<?php get_main_header(); ?>  
+<?php get_main_header(); ?>
 
 <?php
 	// Get the metaBoxes 
@@ -25,120 +25,130 @@
 
 <?php if(!empty($video_link)): ?>
 <div class="modal fade" id="video_project" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      
-      <div class="modal-body">
-        <?php echo video::iframe($video_link); ?>
-      </div>
-      
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+
+            <div class="modal-body">
+                <?php echo video::iframe($video_link); ?>
+            </div>
+
+        </div>
     </div>
-  </div>
 </div>
 <?php endif; ?>
 
 <style>
-    .projectInfo pre {
-        background: none;
-        border: 0;
-        color: white;
-        font-family: 'SWISSRA-BOLD';
-        font-size: 16px;
-        margin-top: -20px;
-        line-height: 36px;
-    }
+.projectInfo pre {
+    background: none;
+    border: 0;
+    color: white;
+    font-family: 'SWISSRA-BOLD';
+    font-size: 16px;
+    margin-top: -20px;
+    line-height: 36px;
+}
+
+[style="font-weight: 400;"] {
+    font-family: 'SWISSRA-light';
+}
 </style>
 
 <section class="projectHeader">
- <div class="container">
-  <div class="col-md-7">
-      <div class="projectInfo">
-          <h1><?php the_title(); ?></h1>
-          <p><pre><?php  echo $little_description; ?></pre> 
-          </p>
-          <ul>
-              <li><a href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>"><img src="<?php echo $vectors; ?>/projectShare/facebook.png" alt=""></a></li>
-              <li><a href="https://twitter.com/intent/tweet?text=<?php echo get_permalink(); ?>"><img src="<?php echo $vectors; ?>/projectShare/twitter.png" alt=""></a></li>
+    <div class="container">
+        <div class="col-md-7">
+            <div class="projectInfo">
+                <h1><?php the_title(); ?></h1>
+                <p>
+                <pre><?php  echo $little_description; ?></pre>
+                </p>
+                <ul>
+                    <li><a href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>"><img
+                                src="<?php echo $vectors; ?>/projectShare/facebook.png" alt=""></a></li>
+                    <li><a href="https://twitter.com/intent/tweet?text=<?php echo get_permalink(); ?>"><img
+                                src="<?php echo $vectors; ?>/projectShare/twitter.png" alt=""></a></li>
 
 
 
-              <li><a href="whatsapp://send?text=<?php echo get_permalink(); ?>" data-action="share/whatsapp/share" ><img src="<?php echo $vectors; ?>/projectShare/whatsapp.png" alt=""></a></li>
-          </ul>
-      </div>
-  </div>
-  <div class="col-md-5">
-      
-      <style>
-          
-          .projectVideo {
-    background: url('');
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-      </style>
-      
-      <div class="projectVideo" style="background-image:url('<?php echo get_the_post_thumbnail_url() ;?>');" >
-<?php if(!empty($video_link)): ?>
-         <a href="javascript:;"  data-toggle="modal" data-target="#video_project">
-              <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png" alt=""></span>
-          </a>
-<?php endif; ?>
-      </div>
-  </div>
-  </div>
+                    <li><a href="whatsapp://send?text=<?php echo get_permalink(); ?>"
+                            data-action="share/whatsapp/share"><img
+                                src="<?php echo $vectors; ?>/projectShare/whatsapp.png" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-5">
+
+            <style>
+            .projectVideo {
+                background: url('');
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            </style>
+
+            <div class="projectVideo" style="background-image:url('<?php echo get_the_post_thumbnail_url() ;?>');">
+                <?php if(!empty($video_link)): ?>
+                <a href="javascript:;" data-toggle="modal" data-target="#video_project">
+                    <span class="videoplay"><img src="<?php echo $vectors; ?>/play.png" alt=""></span>
+                </a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 </section>
 <section class="projectInfo">
     <div class="container">
-    <div class="col-md-8">
-        <div class="projectPost">
-           <?php if ( have_posts() ) { while ( have_posts() ) { the_post();  ?>
+        <div class="col-md-8">
+            <div class="projectPost">
+                <?php if ( have_posts() ) { while ( have_posts() ) { the_post();  ?>
 
-            <?php the_content(); ?>     
-            <?php  } wp_reset_postdata();  }   ?>
-  
+                <?php the_content(); ?>
+                <?php  } wp_reset_postdata();  }   ?>
+
+            </div>
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="projectSidbar">
-            
-            
-            <ul class="project-More-Info">
+        <div class="col-md-4">
+            <div class="projectSidbar">
 
-<?php $spana1 = !empty($agent) ? '<li><span>الشريك</span>'.$agent. '</li>' : '';  echo $spana1; ?>
-<?php $spana2 = !empty($challenge) ? '<li><span>التحدي</span>'.$challenge. '</li>' : '';  echo $spana2; ?>
-<?php $spana3 = !empty($year ) ? '<li><span>سنة الإنتاج</span>  <i class="raleway">'.$year . '</i>       </li>' : '';  echo $spana3; ?>
-<?php $spana4 = !empty($duration  ) ? '<li><span>مدة العمل</span>'.$duration . '</li>' : '';  echo $spana4; ?>
 
-           
-            </ul>
-            
-            
-            
-            
-            
+                <ul class="project-More-Info">
+
+                    <?php $spana1 = !empty($agent) ? '<li><span>الشريك</span>'.$agent. '</li>' : '';  echo $spana1; ?>
+                    <?php $spana2 = !empty($challenge) ? '<li><span>التحدي</span>'.$challenge. '</li>' : '';  echo $spana2; ?>
+                    <?php $spana3 = !empty($year ) ? '<li><span>سنة الإنتاج</span>  <i class="raleway">'.$year . '</i>       </li>' : '';  echo $spana3; ?>
+                    <?php $spana4 = !empty($duration  ) ? '<li><span>مدة العمل</span>'.$duration . '</li>' : '';  echo $spana4; ?>
+
+
+                </ul>
+
+
+
+
+
+            </div>
         </div>
-    </div>
     </div>
 </section>
-<section class="SeeMoreProjects circle_shape"> 
-    
+<section class="SeeMoreProjects circle_shape">
+
     <div class="container">
-        <div class="watchMoreTitl"><h3>شاهد المزيد من أعمالنا</h3></div>
+        <div class="watchMoreTitl">
+            <h3>شاهد المزيد من أعمالنا</h3>
+        </div>
         <div class="col-md-12">
-          
-          
-          
-          <!-- مواضيع ذات صلة  -->
-<?php $related = st_get_related_posts($post->ID,4);?>
-<?php if( $related->have_posts() ) { while( $related->have_posts() ) { $related->the_post(); ?>
 
 
-<div class="col-md-3">
-              
-               <a href="<?php echo get_permalink(); ?>">
-                <div class="moreprojectBox">
-                    <h2><?php echo get_the_title(); ?></h2>
-                </div>
+
+            <!-- مواضيع ذات صلة  -->
+            <?php $related = st_get_related_posts($post->ID,4);?>
+            <?php if( $related->have_posts() ) { while( $related->have_posts() ) { $related->the_post(); ?>
+
+
+            <div class="col-md-3">
+
+                <a href="<?php echo get_permalink(); ?>">
+                    <div class="moreprojectBox">
+                        <h2><?php echo get_the_title(); ?></h2>
+                    </div>
                 </a>
             </div>
 
@@ -150,18 +160,18 @@
 
 
 
-<?php }  wp_reset_postdata(); } ?>
-<!--// مواضيع ذات صلة -->
-          
-          
-          
-          
-          
-      
+            <?php }  wp_reset_postdata(); } ?>
+            <!--// مواضيع ذات صلة -->
+
+
+
+
+
+
         </div>
-        
+
     </div>
-    
+
 </section>
 
 
