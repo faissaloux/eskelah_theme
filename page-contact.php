41816302f965b3006fc04f1f0e2eb6c1e89ac25f
faissaloux <60013703+faissaloux@users.noreply.github.com>
2021-03-08 14:41:39 +0100

    fix: services visual production icon size

--- page-contact.php
+++ page-contact.php
@@ -75,13 +75,8 @@ span.iconic:hover {
         <div class="col-md-3">
             <span class="iconic"><img src="<?php echo $vectors; ?>/contact/4.png" alt=""></span>
             <h1 class="title">الهاتف</h1>
-<<<<<<< HEAD
-            <p class="desc raleway " dir='ltr'>+966567173705</p>
-
-=======
             <p class="desc " dir='ltr'>+966567173705</p>
-            
->>>>>>> d40c39463f5d778115db06576eb415e6bb23fe9a
+
             <a href='tel:+966567173705'> <span>اتصل بنا</span></a>
 
 
@@ -122,7 +117,6 @@ span.iconic:hover {
             <form action="" id="contactForm" class="form">
                 <input type='reset' id='resetform' value='reset' style="display:none;" />
 
-<<<<<<< HEAD
                 <div class="col-md-12">
                     <div class="row">
                         <div class="form-group">
@@ -160,7 +154,6 @@ span.iconic:hover {
             </form>
 
             <img src="<?php echo $vectors; ?>/contact/newsletter-bg.png" alt="">
-=======
           <div class="col-md-12">
            <div class="row">
             <div class="form-group">
@@ -191,7 +184,6 @@ span.iconic:hover {
             </div>
             <div class="form-group text-center"><button class="btn btn">ارسال</button></div>
             <div class="sendresult"></div>
->>>>>>> d40c39463f5d778115db06576eb415e6bb23fe9a
 
         </div>
     </div>
